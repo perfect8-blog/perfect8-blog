1c08175045965c779d761a57df325b35f982047e
0x6273 <0x40@keemail.me>
2025-05-02 13:08:44 +0200

    Add endpoints to get and create blog posts

--- spring-backend/src/main/java/com/perfect8/blog/dto/mapper/PostDtoMapper.java
+++ spring-backend/src/main/java/com/perfect8/blog/dto/mapper/PostDtoMapper.java
@@ -1,5 +1,11 @@
 package com.perfect8.blog.dto.mapper;
 
+import com.perfect8.blog.dto.PostDto;
+import com.perfect8.blog.entity.PostEntity;
+import org.jetbrains.annotations.NotNull;
+
 public class PostDtoMapper {
-    //TODO
+    public static @NotNull PostDto toDto(@NotNull PostEntity entity) {
+        return new PostDto(entity.getTitle(), entity.getBody(), entity.getPublishedAt());
+    }
 }
--- spring-backend/src/main/java/com/perfect8/blog/entity/PostEntity.java
+++ spring-backend/src/main/java/com/perfect8/blog/entity/PostEntity.java
@@ -1,5 +1,45 @@
 package com.perfect8.blog.entity;
 
+import java.time.LocalDateTime;
+
+// TODO: make this a database entity
 public class PostEntity {
-    //TODO
+    /// The database ID of the post
+    private final long id;
+    /// The URL slug
+    private final String slug;
+    /// The title of the post
+    private final String title;
+    /// The body text
+    private final String body;
+    /// When the post was published
+    private final LocalDateTime publishedAt;
+
+    public PostEntity(long id, String slug, String title, String body, LocalDateTime publishedAt) {
+        this.id = id;
+        this.slug = slug;
+        this.title = title;
+        this.body = body;
+        this.publishedAt = publishedAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public LocalDateTime getPublishedAt() {
+        return publishedAt;
+    }
 }
